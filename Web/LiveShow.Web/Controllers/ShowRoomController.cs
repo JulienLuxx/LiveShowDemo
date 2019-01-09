5a1bc6eb3bc16931687142882d2648c6dbf29d74
JulienLuxx <JulienLux@outlook.com>
2019-01-09 11:24:17 +0800

    ImproveCode

---
+++ Web/LiveShow.Web/Controllers/ShowRoomController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiveShow.Service.Dto;
+using LiveShow.Service.Interface;
+using LiveShow.Service.QueryModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveShow.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("API/ShowRoom")]
+    public class ShowRoomController : Controller
+    {
+        private readonly IShowRoomSvc _showRoomSvc;
+        public ShowRoomController(IShowRoomSvc showRoomSvc)
+        {
+            _showRoomSvc = showRoomSvc;
+        }
+
+        [HttpPost("Add")]
+        public JsonResult Add(ShowRoomDto dto)
+        {
+            var result = _showRoomSvc.Add(dto);
+            return Json(result);
+        }
+
+        [HttpPost("Activate")]
+        public async Task<JsonResult> Activate(int id)
+        {
+            var resultTask = _showRoomSvc.Activate(id);
+            return Json(await resultTask);
+        }
+
+        [HttpPost("Page")]
+        public async Task<JsonResult> GetPageAsync(ShowRoomQueryModel qModel)
+        {
+            var resultTask = _showRoomSvc.GetPageDataAsync(id);
+            return Json(await resultTask);
+        }
+
+        [HttpPost("Shutdown")]
+        public async Task<JsonResult> Shutdown(int id)
+        {
+            var resultTask = _showRoomSvc.Shutdown(id);
+            return Json(await resultTask);
+        }
+    }
+}
---
+++ Web/LiveShow.Web/Controllers/ShowRoomViewerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiveShow.Service.Dto;
+using LiveShow.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveShow.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("API/ShowRoomViewer")]
+    public class ShowRoomViewerController : Controller
+    {
+        private readonly IShowRoomViewerSvc _showRoomViewerSvc;
+        public ShowRoomViewerController(IShowRoomViewerSvc showRoomViewerSvc)
+        {
+            _showRoomViewerSvc = showRoomViewerSvc;
+        }
+
+        [HttpPost("Add")]
+        public async Task<JsonResult> Add(ShowRoomViewerDto dto)
+        {
+            var resultTask = _showRoomViewerSvc.Add(dto);
+            return Json(await resultTask);
+        }
+
+        [HttpPost("Remove")]
+        public async Task<JsonResult> Remove(ShowRoomViewerDto dto)
+        {
+            var resultTask = _showRoomViewerSvc.Remove(dto);
+            return Json(await resultTask);
+        }
+    }
+}
