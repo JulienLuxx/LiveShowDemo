6b9d02d65c4a401a3ab9a5244069bf83127297f9
JulienLuxx <JulienLux@outlook.com>
2019-01-15 15:25:02 +0800

    AddMessageCenterWithSignalR&StackExchangeRedis

---
+++ LiveShow.MessageCentre/Startup.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using LiveShow.Core.IOC;
+using LiveShow.Domain;
+using LiveShow.MessageCenter.Hubs;
+using LiveShow.Service.Infrastructure;
+using LiveShow.Service.IOC;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using StackExchange.Redis;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace LiveShow.MessageCenter
+{
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json").Build();
+        }
+
+
+        public Autofac.IContainer ApplicationContainer { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LiveShowDBContext>();
+            services.AddAutoMapper();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "LiveShow"
+                });
+                var xmlFilePaths = new List<string>() {
+                    "LiveShow.MessageCenter.xml",
+                    "LiveShow.Service.xml",
+                    "LiveShow.Core.xml"
+                };
+                foreach (var filePath in xmlFilePaths)
+                {
+                    var xmlFilePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, filePath);
+                    if (File.Exists(xmlFilePath))
+                    {
+                        c.IncludeXmlComments(xmlFilePath);
+                    }
+                }
+            });
+
+            //注册Cors的策略
+            services.AddCors(options => options.AddPolicy("CorsPolicy", policyBuilder => {
+                policyBuilder.AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowAnyOrigin()
+                       .AllowCredentials();
+            }));
+
+            services.AddSignalR(option => option.EnableDetailedErrors = true)
+                .AddMessagePackProtocol()
+                .AddStackExchangeRedis(o =>
+                {
+                    o.ConnectionFactory = async writer =>
+                    {
+                        var config = new ConfigurationOptions
+                        {
+                            AbortOnConnectFail = false
+                        };
+                        config.EndPoints.Add(IPAddress.Loopback, 0);
+                        config.SetDefaultPorts();
+                        config.ChannelPrefix = "SignalTest";
+                        var connection = await ConnectionMultiplexer.ConnectAsync(config, writer);
+                        connection.ConnectionFailed += (_, e) =>
+                        {
+                            Console.WriteLine("Connection Redis failed.");
+                        };
+                        if (!connection.IsConnected)
+                        {
+                            Console.WriteLine("Connection did not connect.");
+                        }
+                        return connection;
+                    };
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<CoreModule>();
+            builder.RegisterModule<ServiceModule>();
+            builder.Populate(services);
+            ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(ApplicationContainer);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            //使用CorsPolicy策略的Cors
+            app.UseCors("CorsPolicy");
+
+            app.UseSignalR(routes => {
+                routes.MapHub<ChatHub>("/chathub");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LiveShow.MessageCenter");
+            });
+
+            Mapper.Initialize(x => x.AddProfile<CustomizeProfile>());
+        }
+    }
+}
