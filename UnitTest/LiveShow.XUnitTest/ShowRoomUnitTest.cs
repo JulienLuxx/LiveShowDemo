94cff64cc056244f1277f3aa79307381c1b5ed2f
JulienLuxx <JulienLux@outlook.com>
2019-02-13 11:30:05 +0800

    CompleteShowRoomUnitTest

--- UnitTest/LiveShow.XUnitTest/ShowRoomUnitTest.cs
+++ UnitTest/LiveShow.XUnitTest/ShowRoomUnitTest.cs
@@ -5,10 +5,13 @@ using LiveShow.Domain.Enum;
 using LiveShow.Service.Dto;
 using LiveShow.Service.Impl;
 using LiveShow.Service.Infrastructure;
+using LiveShow.Service.QueryModel;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using MoqEFCoreExtension;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -22,6 +25,10 @@ namespace LiveShow.XUnitTest
         {
             _sampleList = new List<ShowRoom>()
             {
+                new ShowRoom() { Id=1,Name="Test1",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Default.GetHashCode(),Title="Test1Title",ShowRoomVlewers=new List<ShowRoomVlewer>(){ new ShowRoomVlewer() { UserId=1,ShowRoomId=1,CreateTime=DateTime.Now} } },
+                new ShowRoom() { Id=2,Name="Test2",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Default.GetHashCode(),Title="Test2Title",ShowRoomVlewers=new List<ShowRoomVlewer>(){ new ShowRoomVlewer() { UserId=2,ShowRoomId=2,CreateTime=DateTime.Now} }  },
+                new ShowRoom() { Id=3,Name="Test3",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Activate.GetHashCode(),Title="Test3Title",ShowRoomVlewers=new List<ShowRoomVlewer>(){ new ShowRoomVlewer() { UserId=3,ShowRoomId=3,CreateTime=DateTime.Now} }  },
+                new ShowRoom() { Id=4,Name="Test4",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Disable.GetHashCode(),Title="Test4Title"/*,ShowRoomVlewers=new List<ShowRoomVlewer>(){ new ShowRoomVlewer() { UserId=4,ShowRoomId=4,CreateTime=DateTime.Now} } */ }
             };
         }
         [Fact]
@@ -41,8 +48,55 @@ namespace LiveShow.XUnitTest
         }
 
         [Fact]
+        public async Task ActivateTest()
+        {
+            var mockSet = new Mock<DbSet<ShowRoom>>().SetupList(_sampleList);
+            var mockContext = new Mock<LiveShowDBContext>();
+            mockContext.Setup(x => x.ShowRoom).Returns(mockSet.Object);
+            var mockSvc = new ShowRoomSvc(mockContext.Object);
+            await mockSvc.Activate(4);
+
+            var data = mockContext.Object.ShowRoom.Where(x => x.Id == 4).FirstOrDefault();
+            Assert.Equal(ShowRoomStatusEnum.Activate.GetHashCode(), data.Status);
+            mockContext.Verify(x => x.SaveChanges(), Times.Once());
+        }
+
+        [Fact]
         public async Task GetPageDataTest()
         {
+            var mockSet = new Mock<DbSet<ShowRoom>>().SetupList(_sampleList);
+            var mockContext = new Mock<LiveShowDBContext>();
+            mockContext.Setup(x => x.ShowRoom).Returns(mockSet.Object);
+            var mockSvc = new ShowRoomSvc(mockContext.Object);
+            var result = await mockSvc.GetPageDataAsync(new ShowRoomQueryModel());
+            Assert.Equal(4, result.List.Count());
+        }
+
+        [Fact]
+        public async Task GetSingleDataByUserIdTest()
+        {
+            Mapper.Initialize(x => x.AddProfile<CustomizeProfile>());
+            var mockSet = new Mock<DbSet<ShowRoom>>().SetupList(_sampleList);
+            var mockContext = new Mock<LiveShowDBContext>();
+            mockContext.Setup(x => x.ShowRoom).Returns(mockSet.Object);
+            var mockSvc = new ShowRoomSvc(mockContext.Object);
+            var result = await mockSvc.GetSingleDataByUserIdAsync(1);
+            Assert.Equal(1, result.Data.Id);
+        }
+
+        [Fact]
+        public async Task ShutdownTest()
+        {
+            var mockSet = new Mock<DbSet<ShowRoom>>().SetupList(_sampleList);
+            var mockContext = new Mock<LiveShowDBContext>();
+            mockContext.Setup(x => x.ShowRoom).Returns(mockSet.Object);
+            var mockSvc = new ShowRoomSvc(mockContext.Object);
+            await mockSvc.Shutdown(3);
+
+            var data = mockContext.Object.ShowRoom.Where(x => x.Id == 3).FirstOrDefault();
+            Assert.Equal(ShowRoomStatusEnum.Disable.GetHashCode(), data.Status);
+            Assert.True(data.ShowRoomVlewers.Count() == 0);
+            mockContext.Verify(x => x.SaveChanges(), Times.Once);
         }
     }
 }
