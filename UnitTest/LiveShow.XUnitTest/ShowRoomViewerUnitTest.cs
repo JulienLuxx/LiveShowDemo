5fdc4df5cf57146a70a0c7618f54d818755e59f0
JulienLuxx <JulienLux@outlook.com>
2019-02-13 15:03:47 +0800

    CompleteShowRoomViewerUnitTest

--- UnitTest/LiveShow.XUnitTest/ShowRoomViewerUnitTest.cs
+++ UnitTest/LiveShow.XUnitTest/ShowRoomViewerUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using LiveShow.Domain;
 using LiveShow.Domain.Entitis;
+using LiveShow.Domain.Enum;
 using LiveShow.Service.Dto;
 using LiveShow.Service.Impl;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,7 @@ namespace LiveShow.XUnitTest
     public class ShowRoomViewerUnitTest
     {
         private List<ShowRoomViewer> _sampleList;
+        private List<ShowRoom> _sampleRoomList;
         public ShowRoomViewerUnitTest()
         {
             _sampleList = new List<ShowRoomViewer>()
@@ -25,11 +27,31 @@ namespace LiveShow.XUnitTest
                     ShowRoomId=1,UserId=1
                 },
             };
+            _sampleRoomList = new List<ShowRoom>()
+            {
+                new ShowRoom() { Id=1,Name="Test1",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Default.GetHashCode(),Title="Test1Title",ShowRoomVlewers=new List<ShowRoomViewer>(){ new ShowRoomViewer() { UserId=1,ShowRoomId=1,CreateTime=DateTime.Now} } },
+                new ShowRoom() { Id=2,Name="Test2",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Default.GetHashCode(),Title="Test2Title",ShowRoomVlewers=new List<ShowRoomViewer>(){ new ShowRoomViewer() { UserId=2,ShowRoomId=2,CreateTime=DateTime.Now} }  },
+                new ShowRoom() { Id=3,Name="Test3",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Activate.GetHashCode(),Title="Test3Title",ShowRoomVlewers=new List<ShowRoomViewer>(){ new ShowRoomViewer() { UserId=3,ShowRoomId=3,CreateTime=DateTime.Now} }  },
+                new ShowRoom() { Id=4,Name="Test4",IsDeleted=false,CreateTime=DateTime.Now,LastActivateTime=DateTime.Now,Status=ShowRoomStatusEnum.Disable.GetHashCode(),Title="Test4Title"/*,ShowRoomVlewers=new List<ShowRoomVlewer>(){ new ShowRoomVlewer() { UserId=4,ShowRoomId=4,CreateTime=DateTime.Now} } */ }
+            };
         }
 
         [Fact]
         public async Task AddTest()
-        { }
+        {
+            var mockSet = new Mock<DbSet<ShowRoomViewer>>().SetupList(_sampleList);
+            var mockRoomSet = new Mock<DbSet<ShowRoom>>().SetupList(_sampleRoomList);
+            var mockContext = new Mock<LiveShowDBContext>();
+            mockContext.Setup(x => x.ShowRoomViewer).Returns(mockSet.Object);
+            mockContext.Setup(x => x.ShowRoom).Returns(mockRoomSet.Object);
+            var mockSvc = new ShowRoomViewerSvc(mockContext.Object);
+
+            var dto = new ShowRoomViewerDto() { UserId = 2, ShowRoomId = 2 };
+            await mockSvc.Add(dto);
+
+            mockSet.Verify(x => x.Add(It.IsAny<ShowRoomViewer>()), Times.Once());
+            mockContext.Verify(x => x.SaveChanges(), Times.Once());
+        }
 
         [Fact]
         public async Task RemoveTest()
